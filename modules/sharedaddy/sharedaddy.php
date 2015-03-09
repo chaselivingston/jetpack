8dc8e95d48fc46fc8814f5b71a9116d9517b19e2
Chase Livingston <chaselivingston@gmail.com>
2015-03-08 18:38:25 -1000

    Echo get_options() function

--- modules/sharedaddy/sharedaddy.php
+++ modules/sharedaddy/sharedaddy.php
@@ -40,7 +40,7 @@ function sharing_meta_box_content( $post ) {
 	$disabled = get_post_meta( $post->ID, 'sharing_disabled', true ); ?>
 
 	<p>
-		<?php echo $post->ID; ?>
+		<?php echo get_option( 'page_for_posts' ); ?>
 		<label for="enable_post_sharing">
 			<input type="checkbox" name="enable_post_sharing" id="enable_post_sharing" value="1" <?php checked( !$disabled ); ?>>
 			<?php _e( 'Show sharing buttons.' , 'jetpack'); ?>
